1d1c34c072935c2ce336e911f7f81372e4e8dd47
jpinedadev <jpinedadeveloper@gmail.com>
2022-08-14 16:36:36 -0500

    initial commit

---
+++ src/componentes/Contador.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react"
+import '../hojas-de-estilo/Contador.css'
+
+function Contador({ numClics }){
+    return(
+        <div className='contador'>
+            {numClics}
+        </div>
+        )
+}
+
+export default Contador
